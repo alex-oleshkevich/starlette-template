da4bbfa8312ac57d8f7085abc80c9f78252f0f6a
alex.oleshkevich <alex.oleshkevich@gmail.com>
2024-11-15 23:09:51 +0100

    remove seeds

--- app/cli/seed.py
+++
@@ -1,9 +0,0 @@
-import click
-
-from app.config.seed import seed_database
-
-
-@click.command("seed")
-def seed_command() -> None:
-    """Seed the database with initial data."""
-    seed_database()

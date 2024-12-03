12583f33b6d8fab02d5f5e98ac690d2cffc2f6a5
alex.oleshkevich <alex.oleshkevich@gmail.com>
2024-12-03 19:25:52 +0100

    setup logging

--- app/config/logging.py
+++ app/config/logging.py
@@ -8,19 +8,20 @@ def configure_logging() -> None:
         {
             "version": 1,
             "disable_existing_loggers": False,
+            "root": {
+                "level": settings.log_level,
+                "handlers": ["console"],
+            },
             "formatters": {
                 "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
             },
             "handlers": {
-                "default": {
-                    "level": "DEBUG",
+                "console": {
                     "formatter": "standard",
                     "class": "logging.StreamHandler",
                     "stream": "ext://sys.stderr",
                 },
             },
-            "loggers": {
-                "": {"level": settings.log_level, "handlers": ["default"], "propagate": False},
-            },
+            "loggers": {},
         }
     )
--- app/http/web/internal/routes.py
+++ app/http/web/internal/routes.py
@@ -1,3 +1,5 @@
+import logging
+
 from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
 from starlette_dispatch import RouteGroup
@@ -5,10 +7,12 @@ from starlette_dispatch import RouteGroup
 from app.http.dependencies import Settings
 
 routes = RouteGroup()
+logger = logging.getLogger(__name__)
 
 
 @routes.get("/version")
 async def version_view(request: Request, settings: Settings) -> Response:
+    logger.info("Version view")
     return JSONResponse(
         dict(
             app_env=settings.app_env,
